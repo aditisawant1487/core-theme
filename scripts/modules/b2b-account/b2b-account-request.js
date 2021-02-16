3c182a957749dfd99d1a1432a4f2b77c3806bfff
prakhar308 <khandelwalprakhar30@gmail.com>
2021-02-16 19:33:57 +0530

    COM-2886: Added success popup message and B2BEnabled condition to display account request link
    - If the request is sent successfully, a popup message is displayed and then the user is redirected to home page.

--- scripts/modules/b2b-account/b2b-account-request.js
+++ scripts/modules/b2b-account/b2b-account-request.js
@@ -1,26 +1,25 @@
-define(["modules/jquery-mozu", "hyprlive", "modules/models-b2b-account",], function ($, Hypr, B2BAccountModels) {
+define(["modules/jquery-mozu", "hyprlive", "modules/models-b2b-account", 'hyprlivecontext'], function ($, Hypr, B2BAccountModels, HyprLiveContext) {
 
   $(document).ready(function () {
     var isFormValid;
 
-    var validateEmail = function (email) {
+    function isEmailValid(email) {
       var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
       return emailReg.test(email);
     }
 
-    var validateInput = function () {
+    function validateInput() {
       isFormValid = true;
       // perform validation
       var formValues = $('#account-request').serializeArray();
       formValues.forEach(formValue => {
-
         var { name, value } = formValue;
 
         if (value == "" && name != "taxId") { // taxId is not a required field
           isFormValid = false;
           // show validation message
           $(`[data-mz-validationmessage-for="${name}"]`).show().text(Hypr.getLabel(`${name}Missing`));
-        } else if (name == "userEmail" && !validateEmail(value)) {
+        } else if (name == "userEmail" && !isEmailValid(value)) {
           isFormValid = false;
           $(`[data-mz-validationmessage-for="${name}"]`).show().text(Hypr.getLabel(`${name}InValid`));
         } else {
@@ -30,7 +29,16 @@ define(["modules/jquery-mozu", "hyprlive", "modules/models-b2b-account",], funct
       });
     }
 
-    var requestAccountApiCall = function () {
+    function showErrorMessage(message) {
+      $('[data-mz-role="popover-message"]').html('<span class="mz-validationmessage">' + message + '</span>');
+    }
+
+    function showSuccessMessage() {
+      $('#b2b-success-modal').css('display', 'block');
+    }
+
+    function requestAccountApiCall() {
+      $("#requestAccount").addClass("is-loading");
       // get value from form
       var requestAccount = JSON.parse(JSON.stringify({
         "users": [
@@ -51,28 +59,24 @@ define(["modules/jquery-mozu", "hyprlive", "modules/models-b2b-account",], funct
         "companyOrOrganization": $("#companyName").val(),
         "accountType": "B2B"
       }));
-      $("#requestAccount").addClass("is-loading");
       // create model
       var apib2bAccount = new B2BAccountModels.b2bAccount(requestAccount);
       // call api
       apib2bAccount.apiModel.create().then(function () {
         $("#requestAccount").removeClass("is-loading");
-        displayApiMessage("success", "Account Request Sent Successfully.");
         // clear form
         $('#account-request')[0].reset();
+        showSuccessMessage();
       }, function (error) {
         $("#requestAccount").removeClass("is-loading");
-        displayApiMessage("error", error.message);
+        showErrorMessage(error.message);
       });
     }
 
-    var displayApiMessage = function(messageType, message) {
-      if(messageType == "error") {
-        $('[data-mz-role="popover-message"]').html('<span class="mz-validationmessage">' + message + '</span>');
-      } else {
-        $('[data-mz-role="popover-message"]').html('<span>' + message + '</span>');  
-      }
-    }
+    $('#b2b-close-modal').click(function() {
+      $('#b2b-success-modal').css('display', 'none');
+      window.location.href = "/";
+    })
 
     $("#account-request").submit(function (e) {
       e.preventDefault();
